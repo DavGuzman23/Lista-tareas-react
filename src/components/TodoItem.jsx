49dc61f13b30c2a16a8221f2c7a74cf6fe4a5509
DavGuzman23 <gummer2310@gmail.com>
2024-01-17 13:44:57 +0100

    Anotaciones

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,16 +1,23 @@
 import React from 'react'
 
+// Todo item component
 export function TodoItem( {todo, toggleTodo} ) {
+    // Destructuring todo props
     const {id, task, completed} = todo
-
+ 
+    // Handle todo click function
     const handleTodoClick = () => {
+        // Toggle todo by id
         toggleTodo(id)
     }
-
+ 
+  // Return JSX for todo item
   return (
     <li>
+        {/* Checkbox input for todo */}
         <input type='checkbox' checked= {completed} onChange={handleTodoClick}/>
+        {/* Todo task */}
         {task}
     </li>
   )
-}
+ }
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -2,10 +2,13 @@
 import React from 'react'
 import { TodoItem } from './TodoItem'
 
+// Export TodoList component
 export function TodoList({ todos, toggleTodo }) {
+  // Map over todos and render TodoItem for each todo
   return (
     <ul>
-      { todos.map ((todo) => (
+      {todos.map((todo) => (
+        // Render TodoItem with unique key, todo, and toggleTodo props
         <TodoItem key= {todo.id} todo={todo} toggleTodo={toggleTodo}/>
       ))}
     </ul>
